770ae054e50c1f259d096670750c5ac1e2a0cd88
cksgud0403 <cksgud0403@naver.com>
2024-10-09 16:29:54 +0900

    feat: initial commit

---
+++ src/main/java/com/example/lifelog/LifelogApplication.java
@@ -0,0 +1,13 @@
+package com.example.lifelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifelogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifelogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lifelog/LifelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lifelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifelogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
